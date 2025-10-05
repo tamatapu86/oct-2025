6f6c42551c4edd44068a2d93f983edcc8022c0f5
tamatapu86 <mohantamatapu@gmail.com>
2025-10-05 12:08:55 +0530

    Add files via upload

---
+++ transfer.py
@@ -0,0 +1,45 @@
+#break # syntax
+# for item in iterable:
+#     if condition:
+#         break
+
+# employee_data = ["rakesh","shiva","ravi","mohan","krishna"]
+# for item in employee_data:
+#     if item == "mohan":
+#        print(item)
+#        break
+#     print(item)
+# print("last iteration", item)
+
+# for   last itreation",i)
+
+#continue 3 synatx
+
+# for item in iterable:
+#     if condition:
+#         continue
+
+# for i in range(11):
+#     if i == 5:
+#         continue
+#     print(i)
+
+# print(f'last iteration{i}')
+
+# products = ["ok","ok","ok","ok","ok","ok","ok","defect","ok","ok","defect","ok"]
+# for i in products:
+#     if i == "defect":
+#         continue
+#     print(i)
+# print(f'last iteration {i}')
+
+# pass : it is null operation
+
+# condition # syntax
+# if condition:
+# pass
+
+for i in range(10):
+    pass
+print("last iteration {i}")
+
